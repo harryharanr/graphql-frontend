d2db59ca4da1e372230bedbcceffc87a6b3bae5f
Hariharan Ramanan <harryharanr@gmail.com>
2020-07-19 15:16:57 +0530

    graphql boilerplate code added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-poc';
+export class AppComponent implements OnInit {
+  rates: any[];
+  loading = true;
+  error: any;
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {}
+
+  getData() {
+    this.apollo
+      .watchQuery({
+        query: gql`
+          {
+            events {
+              title
+            }
+          }
+        `,
+      })
+      .valueChanges.subscribe((result: any) => {
+        console.log(result);
+      });
+  }
 }
